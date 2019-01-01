4e2b36599672cb30895b64b83f93b145b4ab6fc0
thameemk612 <thameemk612@gmail.com>
2019-01-01 14:55:41 +0530

    demo.css added

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Evoke 19</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+   
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+
+   
+
+</head>
+
+<body class="loading">
+    <iframe src="bg/index.html" class="main-frame" frameborder="0"></iframe>
+
+    <div class="menu">
+        <span class="menu-circle"></span>
+        <a href="#" class="menu-link">
+            <span class="menu-icon">
+                <span class="menu-line menu-line-1"></span>
+                <span class="menu-line menu-line-2"></span>
+                <span class="menu-line menu-line-3"></span>
+            </span>
+        </a>
+    </div>
+
+    <div class="menu-overlay">
+        <div class="social_nav">
+            <a class="social_icon" href="#" target="_blank"><img src="img/social_icons/facebook.svg"
+                    alt=""></a>
+            <a class="social_icon" href="#" target="_blank"><img src="img/social_icons/instagram.svg"
+                    alt=""></a>
+            <a class="social_icon" href="#" target="_blank"><img src="img/social_icons/youtube.svg"
+                    alt=""></a>
+
+        </div>
+        <a href="index.html">Home</a>
+        <a id="about">About Us</a>
+       
+        <a id="contact">Contact Us</a>
+         <!-- <a id="credit">Credits</a> -->
+        <div class="overlay_content">
+            <div class="about">
+                <div class="container">
+                    <p class="about_evoke"><h3>TKM College of Engineering, Kollam, Kerala presents <font style="font-weight: bold;">'EVOKE 2019: The dawn of green intelligence,</font> conducted by IEDC and IEEE TKMCE.
+In the present era where the rising concern regarding environmental protection and the need for smart technologies explosively overlap, EVOKE 19 brings out the technical rich minds to the stage.</h3>
+                    </p>
+                    <p class="about_evoke"><!-- Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. --> </p>
+                </div>
+            </div>
+            <div class="contact">
+                <div class="container">
+                    <div class="contact_wrapper">
+                        <div class="mobile_col">
+                            <div class="contact_card">
+                                <img src="contact/neethu.jpg" alt="">
+
+                                <h2>Neethu EV</h2>
+                                <p>CEO IEDC TKMCE</p>
+                                <p>95446 95845</p>
+                                
+                            </div>
+                            <div class="contact_card">
+                                <img src="contact/harry.jpg" alt="">
+
+                                <h2>Harry Paul</h4>
+                                    <p>Chairman IEEE SB TKMCE</p>
+                                    <p>94977 04887</p>
+                                    
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <a href="mailto:evoke2k19@gmail.com"><h3>evoke2k19@gmail.com</h3></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div id="home" class="hero_logo">
+            <img src="img/logo/favicon.png" >
+            <h2>
+                <p>e</p>
+                <p>v</p>
+                <p>o</p>
+                <p>k</p>
+                <p>e</p>
+                <p>1</p>
+                <p>9</p>
+            </h2>
+            <p class="tag">THE DAWN OF GREEN INTELLIGENCE</p>
+            <p class="date">1-3 February</p><br>
+            <h1>Coming Soon....</h1>
+        </div>
+
+        
+    </div>
+  <!--   <nav class="main-nav">
+        <ul>
+            <li><a href="events/" target="_blank"><h3>Events</h3></a></li>
+            <li><a href="workshop/" target="_blank"><h3>Workshops</h3></a></li>
+            <li><a href="hackathon/" target="_blank"><h3>Hackathon</h3></a></li>
+            <li><a href="#" target="_blank"><h3>Cells</h3></a></li>
+            <li><a href="compitions/" target="_blank"><h3>Compitions</h3></a></li>
+             <li><a href="talks/" target="_blank"><h3>Talks</h3></a></li>
+        </ul>
+    </nav> -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.slim.min.js'></script>
+    <script src="js/anime.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/main.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131493860-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-131493860-1');
+</script>
+
+</body>
+
+
+
+</html>
