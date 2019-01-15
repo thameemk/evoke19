6b29cb0617d432a67223dc3941db64586847ea9d
thameemk612 <thameemk612@gmail.com>
2019-01-15 23:29:53 +0530

    image changed

---
+++ talks/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Talks | Evoke '19</title>
+	
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.3.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<!--link rel="stylesheet" type="text/css" href="css/card.css" /-->
+	<link rel="stylesheet" type="text/css" href="css/pattern.css" />
+	<link rel="stylesheet" type="text/css" href="css/material-kit.min.css" />
+
+	
+	<script>
+		if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+			var root = document.getElementsByTagName('html')[0];
+			root.setAttribute('class', 'ff');
+		};
+	</script>
+</head>
+
+<body class="demo-2">
+	<iframe src="../bg/index.html" class="main-frame" frameborder="0"></iframe>
+	<div class="container">
+		<header class="codrops-header">
+			<h1> 
+                <b>Lecture Series</b></h1>
+		</header>
+		<div class="content">
+			<!-- trianglify pattern container -->
+			<div class="pattern pattern--hidden"></div>			
+        </div>
+    </div>
+	<script src="js/core/jquery.min.js" type="text/javascript"></script>
+	<script src="js/core/popper.min.js" type="text/javascript"></script>
+	<script src="js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+	<script src="js/core/moment.min.js"></script>
+</body>
+</html>
