553679644fb271409340469dea8a31d8ece24aa4
thameemk612 <thameemk612@gmail.com>
2019-01-16 22:48:21 +0530

    chat bot

--- live-chat.php
+++ live-chat.php
@@ -3,7 +3,7 @@
 	<title>Live | Evoke'19</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script src="js/chat.js"></script>
 	<style type="text/css">
   body{
@@ -16,6 +16,12 @@
   margin-right: auto;
   width: 50%;
   }
+  .custom2 {
+    color: #fff;
+    background-color: #9c27b0;
+    border-color: #9c27b0;
+    box-shadow: 0 2px 2px 0 rgba(156,39,176,.14), 0 3px 1px -2px rgba(156,39,176,.2), 0 1px 5px 0 rgba(156,39,176,.12);
+}
 		.box1 { width: 90%; margin: 0 auto;height: auto; border: 2px solid black;padding: 10px;	 }
 		/* div { width: 400px;height: 450px;overflow: auto	 } */
 		.userInput{float: left;
@@ -32,7 +38,8 @@
 
 	<div class = 'chatBox'>
 	</div>
-	<input id="input" type="text" style="width: 70%;"> <button id="rec" onclick="send()">send</button>
+	<input id="input" type="text" style="width: 100%;">
+    <!-- <button type="button" class="custom2">send</button>  -->
 </div>
 
 </body>
